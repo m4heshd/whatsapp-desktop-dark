f95265666d03f0f32d36d2ccc4570a48350e61a2
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2019-08-19 12:20:07 +0530

    Binary build script for all platforms

---
+++ build.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const fs = require('fs-extra');
+const {exec} = require("child_process");
+const {compile} = require('nexe');
+
+let distDir = path.join(__dirname, 'dist', 'Windows');
+let out = path.join(distDir, 'WADark.exe');
+let stylePath = path.join(__dirname, 'styles', 'win32');
+let distStylePath = path.join(distDir, 'styles', 'win32');
+let scriptPath = path.join(__dirname, 'run-win32.js');
+let distScriptPath = path.join(distDir, 'run-win32.js');
+let target = 'windows-x86-12.7.0';
+
+switch (process.argv[2].trim()) {
+    case 'win32':
+        runBuild();
+        break;
+    case 'darwin':
+        distDir = path.join(__dirname, 'dist', 'macOS');
+        out = path.join(distDir, 'WADark');
+        stylePath = path.join(__dirname, 'styles', 'darwin');
+        distStylePath = path.join(distDir, 'styles', 'darwin');
+        scriptPath = path.join(__dirname, 'run-darwin.js');
+        distScriptPath = path.join(distDir, 'run-darwin.js');
+        target = 'mac-x64-12.7.0';
+        runBuild();
+        break;
+    default:
+        console.log('\x1b[31m%s\x1b[0m', 'Invalid platform type.\n');
+}
+
+function runBuild() {
+    compile({
+        input: path.join(__dirname, 'index.js'),
+        build: false,
+        output: out,
+        targets: target
+    }).then(() => {
+        try {
+            console.log('Copying resources..');
+            fs.copySync(stylePath, distStylePath);
+            fs.copySync(scriptPath, distScriptPath);
+            switch (process.platform) {
+                case 'win32':
+                    console.log('\x1b[32m%s\x1b[0m', '\nAll done. Run \'WADark.exe\' from dist\\Windows directory.\n');
+                    exec(`start "" "${distDir}"`);
+                    break;
+                case 'darwin':
+                    console.log('\x1b[32m%s\x1b[0m', '\nAll done. Run \'WADark\' from dist\\macOS directory.\n');
+                    exec(`open "${distDir}"`);
+                    break;
+                default:
+                    console.log('\x1b[32m%s\x1b[0m', '\nAll done. Run the executable from dist directory.\n');
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }).catch(err => {
+        console.error(err)
+    });
+}
