c89953c36d446874b39652084a84dfa760f72d32
Mahesh Bandara Wijerathna <m4heshd@gmail.com>
2020-01-28 20:29:25 +0530

    Rename GUI script to represent cross-platform support

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ if (arg && arg === 'cli') {
 } else {
     switch (process.platform) {
         case 'win32':
-            require("./run-win32-gui").startGUI();
+            require("./run-gui").startGUI();
             break;
         case 'darwin':
-            require("./run-win32-gui").startGUI();
+            require("./run-gui").startGUI();
             break;
         default:
             console.log('\x1b[31m%s\x1b[0m', 'This platform is not supported.\n');
